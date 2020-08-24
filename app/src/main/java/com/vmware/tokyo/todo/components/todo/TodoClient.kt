09462e4239ad0f69893a43ab7ecc9544ca4adc94
Dan Carter <danielca@vmware.com>
2020-08-24 10:20:52 +0900

    Remove getTodoClient as it is no longer needed
    Signed-off-by: Robert Gravina <rgravina@vmware.com>

--- app/src/main/java/com/vmware/tokyo/todo/components/todo/TodoClient.kt
+++ app/src/main/java/com/vmware/tokyo/todo/components/todo/TodoClient.kt
@@ -1,21 +1,8 @@
 package com.vmware.tokyo.todo.components.todo
 
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
 interface TodoClient {
     @GET("/todos")
     suspend fun getAll(): List<Todo>
 }
-
-private val client: TodoClient by lazy {
-    val retrofit = Retrofit.Builder()
-        .baseUrl("https://todolistserver.cfapps.io/")
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
-
-    retrofit.create(TodoClient::class.java)
-}
-
-fun getTodoClient() = client
